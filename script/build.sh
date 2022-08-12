610015a7bb69fe74b5e0d81591439e0a7191fb5f
Yudai NAKATA <yudai.nkt@gmail.com>
2022-08-12 11:25:25 +0900

    ci: set up release workflow

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-echo "TODO implement this script."
-echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
-exit 1
+
+mkdir -p dist
+
+TARGET=${TARGET:-x86_64-apple-darwin}
+TAG=${TAG:-canary}
+OS_ARCH_EXT=${OS_ARCH_EXT:-darwin-amd64}
+
+cargo build --release --target "${TARGET}"
+mv "target/${TARGET}/release/gh-form" "./dist/gh-form_${TAG}_${OS_ARCH_EXT}"
