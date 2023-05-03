f13b1a32c697d7c399b172b6a1b0d2757f6ee02f
Yudai NAKATA <yudai.nkt@gmail.com>
2023-05-04 05:13:26 +0900

    test: add a test for template chooser

--- src/issue/config.rs
+++ src/issue/config.rs
@@ -52,3 +52,32 @@ impl Config {
         }
     }
 }
+
+#[cfg(test)]
+mod unit_test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn config() {
+        let body = Config {
+            blank_issues_enabled: false,
+            contact_links: vec![
+                ContactLink {
+                    name: "GitHub Community Support".to_string(),
+                    url: "https://github.com/orgs/community/discussions".to_string(),
+                    about: "Please ask and answer questions here.".to_string(),
+                },
+                ContactLink {
+                    name: "GitHub Security Bug Bounty".to_string(),
+                    url: "https://bounty.github.com/".to_string(),
+                    about: "Please report security vulnerabilities here.".to_string(),
+                },
+            ],
+        };
+        assert_eq!(
+            &body.render().into_string(),
+            r#"<div class="summary"><div><strong class="name">GitHub Community Support</strong><div class="description">Please ask and answer questions here.</div></div><a class="button external" href="https://github.com/orgs/community/discussions" target="_blank" rel="noopener noreferrer">Open</a></div><div class="summary"><div><strong class="name">GitHub Security Bug Bounty</strong><div class="description">Please report security vulnerabilities here.</div></div><a class="button external" href="https://bounty.github.com/" target="_blank" rel="noopener noreferrer">Open</a></div>"#
+        )
+    }
+}
